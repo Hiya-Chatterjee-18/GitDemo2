b99e896cdc8344a066642196ee972dc81b1709b0
Hiya-Chatterjee-18 <hiya2005.chat@gmail.com>
2025-12-13 06:13:28 +0530

    modified to restore

--- okk2.java
+++ okk2.java
@@ -1,6 +1,10 @@
 public class okk2 {
     public static void main(String[] args) {
         System.out.println("Hello2");
+        System.out.println("wow");
+        System.out.println("wow");
+        System.out.println("wow");
+        System.out.println("wow");
         
     }
 }
