fcd7510dd09f2d32f28b77f44083404712ec7708
Hiya-Chatterjee-18 <hiya2005.chat@gmail.com>
2025-12-13 06:44:28 +0530

    Revert "modified to restore"
    This reverts commit b99e896cdc8344a066642196ee972dc81b1709b0.

--- okk2.java
+++ okk2.java
@@ -1,10 +1,6 @@
 public class okk2 {
     public static void main(String[] args) {
         System.out.println("Hello2");
-        System.out.println("wow");
-        System.out.println("wow");
-        System.out.println("wow");
-        System.out.println("wow");
         
     }
 }
